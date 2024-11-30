88d61bb412f1f84fffe79b9d10c5df9c2d95cd9a
MylasJacob123 <mylasjacob18.5@gmail.com>
2024-11-30 15:51:34 +0200

    Protected Routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { useEffect } from "react";
-import Register from "./components/register";
-import Login from "./components/login";
-import ForgotPassword from "./components/forgotpassword";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+
 import Home from "./components/home";
-import Facilities from "./components/facilities";
 import Rooms from "./components/rooms";
+import Facilities from "./components/facilities";
 import About from "./components/about";
 import Contact from "./components/contact";
-import "./App.css";
 import RoomDisplay from "./components/roomdisplay";
 import User from "./components/userprofile";
 import PaymentPage from "./components/paymentsummarypage";
 import AdminBookings from "./components/adminpage";
-import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 import Profile from "./components/userprofile";
-import { useDispatch } from "react-redux";
-import { initializeUser } from "./redux/authSlice";
+import Register from "./components/register";
+import Login from "./components/login";
+import ForgotPassword from "./components/forgotpassword";
 import TermsAndConditions from "./components/TermsAndConditions";
+import { initializeUser } from "./redux/authSlice";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
-  
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated); // Assuming auth state contains isAuthenticated
+
   const initialOptions = {
     "client-id":
       "AfIAqx5qwADS2y3HBA3G9jY9LTQxgY71yk1o5OT6ca0OwgiOfGQ2hUnNVYNRVYUDF3MgjtvljjF2m_iN",
@@ -28,10 +31,8 @@ function App() {
     currency: "USD",
   };
 
-  const dispatch = useDispatch();
-
   useEffect(() => {
-    dispatch(initializeUser()); 
+    dispatch(initializeUser());
   }, [dispatch]);
 
   return (
@@ -44,15 +45,43 @@ function App() {
             <Route path="/facilities" element={<Facilities />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/user" element={<User />} />
-            <Route path="/admin" element={<AdminBookings />} />
-            <Route path="/userprofile" element={<Profile />} />
+            <Route path="/roomdisplay" element={<RoomDisplay />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
             <Route path="/forgot" element={<ForgotPassword />} />
-            <Route path="/roomdisplay" element={<RoomDisplay />} />
-            <Route path="/paymentsummary" element={<PaymentPage />} />
             <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+            <Route
+              path="/user"
+              element={
+                <PrivateRoute isAuthenticated={isAuthenticated}>
+                  <User />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/admin"
+              element={
+                <PrivateRoute isAuthenticated={isAuthenticated}>
+                  <AdminBookings />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/userprofile"
+              element={
+                <PrivateRoute isAuthenticated={isAuthenticated}>
+                  <Profile />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/paymentsummary"
+              element={
+                <PrivateRoute isAuthenticated={isAuthenticated}>
+                  <PaymentPage />
+                </PrivateRoute>
+              }
+            />
           </Routes>
         </PayPalScriptProvider>
       </BrowserRouter>
